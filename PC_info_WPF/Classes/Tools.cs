0f3542553a6b547279d9e43028a4973d55c03f70
ZakharovIA <ZakharovIA@nornik.ru>
2019-03-07 15:45:58 +0300

    Init

---
+++ PC_info_WPF/Classes/Tools.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace PC_info_WPF.Classes
+{
+    public static class Tools
+    {
+        public static List<string> GetHardwareInfo(string WIN32_Class, string ClassItemField)
+        {
+            List<string> result = new List<string>();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + WIN32_Class);
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    result.Add(obj[ClassItemField].ToString().Trim());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                Console.WriteLine($"METHOD: {ex.TargetSite}");
+                Console.WriteLine($"STACK TRACE: {ex.StackTrace}");
+            }
+
+            return result;
+        }
+
+        public static void OutputResult(string info, List<string> result)
+        {
+            if (info.Length > 0)
+                Console.WriteLine(info);
+
+            if (result.Count > 0)
+            {
+                for (int i = 0; i < result.Count; ++i)
+                    Console.WriteLine(result[i]);
+            }
+        }
+
+        public static string ListToString(List<string> ts)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in ts)
+            {
+                result.AppendLine(item);
+            }
+
+            return result.ToString().Substring(0, result.Length - 2);  // Delete last '\n'
+        }
+    }
+}
---
+++ PC_info_WPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Management;
+using PC_info_WPF.Classes;
+
+namespace PC_info_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Cpu_name_value.Text = Tools.ListToString(Tools.GetHardwareInfo("Win32_Processor", "Name"));
+            Cpu_status_value.Text = Tools.ListToString(Tools.GetHardwareInfo("Win32_Processor", "Status"));
+            //Cpu_temp_value.Text =
+            Cpu_frequency_value.Text = Tools.ListToString(Tools.GetHardwareInfo("Win32_Processor", "CurrentClockSpeed")) + " Hz";
+            Cpu_family_value.Text = Tools.ListToString(Tools.GetHardwareInfo("Win32_Processor", "Family"));
+            Cpu_cur_volt_value.Text = Tools.ListToString(Tools.GetHardwareInfo("Win32_Processor", "CurrentVoltage"));
+            Cpu_cpu_status_value.Text = Tools.ListToString(Tools.GetHardwareInfo("Win32_Processor", "CpuStatus"));
+        }
+    }
+}
